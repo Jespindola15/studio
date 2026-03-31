aacb8b003bb58f725835562d289bf09808964585
Joaquin Espindola <JoaquinEspindola53@gmail.com>
2026-03-31 23:47:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7552901756-d856d",
+  "appId": "1:364440573905:web:f5f868ec8f91847b0272f0",
+  "apiKey": "AIzaSyADnou7QtbGadWHejUEUKXvu3v9_Y7Lixs",
+  "authDomain": "studio-7552901756-d856d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "364440573905"
+};
